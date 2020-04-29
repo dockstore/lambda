1778d3a6d070ff9250f52235f3045b200dd27301
Andrew Duncan <andrew.duncan@oicr.on.ca>
2020-04-29 11:51:30 -0400

    added comment about unsupported events

--- upsertGitHubTag-f79c4f36-3e61-43f4-8f6c-0b2e0e9774d7/deployment/index.js
+++ upsertGitHubTag-f79c4f36-3e61-43f4-8f6c-0b2e0e9774d7/deployment/index.js
@@ -115,7 +115,13 @@ function processEvent(event, callback) {
     
     var path = process.env.API_URL;
 
-    if (!('repository' in body) || !('ref' in body)) {
+    /**
+     * We only handle push events, of which there are many subtypes. Unfortunately, the only way to differentiate between them is to look
+     * for expected fields. There are no enums for push events subtypes.
+     * 
+     * If an event is deemed not supported, we will return a success and print a message saying the event is not supported.
+     */
+    if (!('repository' in body) || !('ref' in body) || !('created' in body) || !('deleted' in body) || !('pusher' in body)) {
         console.log('Event is not supported')
         callback(null, {"statusCode": 200, "body": "Currently, this lambda does not support this event type from GitHub."});
     }
