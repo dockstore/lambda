ce67411bb09d23e08c461e6f71cfb67cddb4c89e
richard-hansen <richie.hansen@gmail.com>
2021-02-19 12:13:07 -0500

    Renamed tests, added lambda test

---
+++ fedramp-integrated-inventory-workbook/tests/test_subnet_mapper.py
@@ -0,0 +1,23 @@
+import json
+import os
+import pytest
+from inventory.mappers import SubnetDataMapper
+
+
+@pytest.fixture()
+def full_subnet_config():
+    with open(os.path.join(os.path.dirname(__file__), "sample_config_query_results/sample_subnet.json")) as file_data:
+        file_contents = file_data.read()
+
+    return json.loads(file_contents)
+
+
+def test_given_resource_type_is_not_subnet_then_empty_array_is_returned(full_subnet_config):
+    full_subnet_config["resourceType"] = "NOT subnet"
+
+    mapper = SubnetDataMapper()
+
+    assert mapper.map(full_subnet_config) == []
+
+    full_subnet_config["resourceType"] = "AWS::EC2::subnet"
+    assert len(mapper.map(full_subnet_config)) > 0, "Resource should have been mapped"
