c59f0cc89bef11b70d58cc9b0f0e5a12622bb275
richard-hansen <richie.hansen@gmail.com>
2021-02-19 15:17:37 -0500

    Added subnet testing, this combines with route table testing as well

--- fedramp-integrated-inventory-workbook/tests/test_subnet_mapper.py
+++ fedramp-integrated-inventory-workbook/tests/test_subnet_mapper.py
@@ -12,6 +12,14 @@ def full_subnet_config():
     return json.loads(file_contents)
 
 
+@pytest.fixture()
+def full_route_table_config():
+    with open(os.path.join(os.path.dirname(__file__), "sample_config_query_results/sample_route_table.json")) as file_data:
+        file_contents = file_data.read()
+
+    return json.loads(file_contents)
+
+
 def test_given_resource_type_is_not_subnet_then_empty_array_is_returned(full_subnet_config):
     full_subnet_config["resourceType"] = "NOT subnet"
 
@@ -21,3 +29,7 @@ def test_given_resource_type_is_not_subnet_then_empty_array_is_returned(full_sub
 
     full_subnet_config["resourceType"] = "AWS::EC2::subnet"
     assert len(mapper.map(full_subnet_config)) > 0, "Resource should have been mapped"
+
+
+def test_subnet_publicity_based_on_route_tables(full_subnet_config, full_route_table_config):
+    pass
