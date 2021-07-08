d81c9819bf13a4b60e6bb4d256a4a429ab252faf
richard-hansen <richie.hansen@gmail.com>
2021-07-08 10:54:23 -0400

    Reader now leverages an environment variable for regions

--- fedramp-integrated-inventory-workbook/deployment/inventory/readers.py
+++ fedramp-integrated-inventory-workbook/deployment/inventory/readers.py
@@ -21,40 +21,43 @@ class AwsConfigInventoryReader():
         self._mappers: List[DataMapper] = mappers
 
     # Moved into it's own method to make it easier to mock boto3 client
-    def _get_config_client(self, sts_response) -> boto3.client:
+    def _get_config_client(self, sts_response, region: str) -> boto3.client:
         sts_response = sts_response.get_frozen_credentials()
         return boto3.client('config',
                             aws_access_key_id=sts_response.access_key,
                             aws_secret_access_key=sts_response.secret_key,
                             aws_session_token=sts_response.token,
-                            region_name=os.environ['AWS_REGION'])
+                            region_name=region)
 
-    def _get_resources_from_account(self, account_id: str) -> Iterator[List[str]]:
+    def _get_resources_from_account(self, account_id: str, region_list: List[str]) -> Iterator[List[str]]:
         try:
             _logger.info(f"assuming role on account {account_id}")
 
             # get the credentials for the current role assumed by the lambda
             sts_response = boto3.Session().get_credentials()
-            config_client = self._get_config_client(sts_response)
 
-            next_token: str = ''
-            while True:
-                resources_result = config_client.select_resource_config(
-                    Expression="SELECT arn, resourceName, resourceId, resourceType, configuration, supplementaryConfiguration, configurationStateId, tags, awsRegion "
-                               "WHERE resourceType IN ('AWS::EC2::Instance', 'AWS::ElasticLoadBalancingV2::LoadBalancer', "
-                               "'AWS::ElasticLoadBalancing::LoadBalancer', 'AWS::RDS::DBInstance', "
-                               "'AWS::Lambda::Function', 'AWS::EC2::VPC', 'AWS::S3::Bucket')",
-                    NextToken=next_token)
+            for region in region_list:
+                config_client = self._get_config_client(sts_response, region)
 
-                next_token = resources_result.get('NextToken', '')
-                results: List[str] = resources_result.get('Results', [])
+                next_token: str = ''
+                while True:
+                    resources_result = config_client.select_resource_config(
+                        Expression="SELECT arn, resourceName, resourceId, resourceType, configuration, supplementaryConfiguration, configurationStateId, tags, awsRegion "
+                                   "WHERE resourceType IN ('AWS::EC2::Instance', 'AWS::ElasticLoadBalancingV2::LoadBalancer', "
+                                   "'AWS::ElasticLoadBalancing::LoadBalancer', 'AWS::RDS::DBInstance', "
+                                   "'AWS::Lambda::Function', 'AWS::EC2::VPC', 'AWS::S3::Bucket')",
+                        NextToken=next_token)
 
-                _logger.debug(f"page returned {len(results)} and next token of '{next_token}'")
+                    next_token = resources_result.get('NextToken', '')
+                    results: List[str] = resources_result.get('Results', [])
 
-                yield results
+                    _logger.debug(f"page returned {len(results)} and next token of '{next_token}'")
 
-                if not next_token:
-                    break
+                    yield results
+
+                    if not next_token:
+                        break
+
         except ClientError as ex:
             _logger.error("Received error: %s while retrieving resources from account %s, moving onto next account.", ex, account_id,
                           exc_info=True)
@@ -71,11 +74,12 @@ class AwsConfigInventoryReader():
 
         all_inventory: List[InventoryData] = []
         accounts = json.loads(os.environ["ACCOUNT_LIST"])
+        region_list = accounts["regions"]
 
         for account in accounts:
-            _logger.info(f"retrieving inventory for account {account['id']}")
+            _logger.info(f"retrieving inventory for account {account['id']} in regions {region_list}")
 
-            for resource_list_page in self._get_resources_from_account(account["id"]):
+            for resource_list_page in self._get_resources_from_account(account["id"], region_list):
                 _logger.debug(f"current page of inventory contained {len(resource_list_page)} items from AWS Config")
 
                 for raw_resource in resource_list_page:
