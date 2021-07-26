24abed690db409e91c98357e95a33fdef762f994
Gary Luu <garylau.work@gmail.com>
2021-07-26 15:49:26 -0400

    Add test

--- checkUrlExists/lambda/tests/unit/test-handler.js
+++ checkUrlExists/lambda/tests/unit/test-handler.js
@@ -7,8 +7,12 @@ describe("Tests index", function () {
   beforeEach(function () {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
   });
-  it("verifies successful response", async () => {
-    const url = "https://docs.aws.amazon.com/lambda/latest/dg/welcome.html";
+  it("verifies successful response from http", async () => {
+    const url = "http://docs.aws.amazon.com/lambda/latest/dg/welcome.html";
+    await setupTest(url, true);
+  });
+  it("verifies successful response from https", async () => {
+    const url = "https://www.google.ca";
     await setupTest(url, true);
   });
   it("verifies unsuccessful response", async () => {
@@ -20,6 +24,16 @@ describe("Tests index", function () {
     const url =
       "https://dcc.icgc.org/api/v1/download?fn=/PCAWG/reference_data/data_for_testing/HCC1143_ds/HCC1143.bam";
     await setupTest(url, true);
+  });
+  it("verifies successful response from ftp file", async () => {
+    const url =
+      "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/CHANGELOG";
+    await setupTest(url, true);
+  });
+  it("verifies unsuccessful response from ftp file", async () => {
+    const url =
+      "ftp://ftp.this.is.fake.or.private.1000genomes.ebi.ac.uk/vol1/ftp/CHANGELOG";
+    await setupTest(url, false);
   });
 });
 
