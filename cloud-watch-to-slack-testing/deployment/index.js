65cf9c206c7de44cca7a00fb8a656beb72525129
Kathy Tran <kathylevtran@gmail.com>
2022-06-20 11:21:19 -0400

    Fix again

--- cloud-watch-to-slack-testing/deployment/index.js
+++ cloud-watch-to-slack-testing/deployment/index.js
@@ -216,7 +216,7 @@ function cloudWatchEventBridgeAlarmMessageText(message) {
   const alarmName = message.detail.alarmName;
   const newState = message.detail.state.value;
   const newStateReason = message.detail.state.reason;
-  if (message.state.value === "ALARM") {
+  if (newState === "ALARM") {
     return `${alarmName} state is now ${newState}: ${newStateReason}`;
   } else {
     return "";
