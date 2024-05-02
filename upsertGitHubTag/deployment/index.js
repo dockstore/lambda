c05d328b13593ca2b16f42f32d02028e992d6b21
hyunnaye <nayeonhyun802@gmail.com>
2024-05-02 16:13:24 -0400

    lint

--- upsertGitHubTag/deployment/index.js
+++ upsertGitHubTag/deployment/index.js
@@ -268,7 +268,8 @@ function logPayloadToS3(body, deliveryId) {
   if (process.env.BUCKET_NAME) {
     const date = new Date();
     const uploadYear = date.getFullYear();
-    const uploadMonth = (date.getMonth() + 1 < 10 ? "0" : "") + (date.getMonth() + 1); // ex. get 05 instead of 5 for May
+    const uploadMonth =
+      (date.getMonth() + 1 < 10 ? "0" : "") + (date.getMonth() + 1); // ex. get 05 instead of 5 for May
     const uploadDate = (date.getDate() < 10 ? "0" : "") + date.getDate(); // ex. get 05 instead of 5 for the 5th date
     const bucketPath = `${uploadYear}-${uploadMonth}-${uploadDate}/${deliveryId}`;
 
