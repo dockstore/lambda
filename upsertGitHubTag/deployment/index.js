f6e708e020a1ae548f68886fafcca6e9c332fcd7
hyunnaye <nayeonhyun802@gmail.com>
2024-05-03 11:21:49 -0400

    padstart

--- upsertGitHubTag/deployment/index.js
+++ upsertGitHubTag/deployment/index.js
@@ -269,8 +269,8 @@ function logPayloadToS3(body, deliveryId) {
     const date = new Date();
     const uploadYear = date.getFullYear();
     const uploadMonth =
-      (date.getMonth() + 1).padStart(2, '0'); // ex. get 05 instead of 5 for May
-    const uploadDate = date.getDate().toString().padStart(2, '0'); // ex. get 05 instead of 5 for the 5th date
+      (date.getMonth() + 1).padStart(2, "0"); // ex. get 05 instead of 5 for May
+    const uploadDate = date.getDate().toString().padStart(2, "0"); // ex. get 05 instead of 5 for the 5th date
     const bucketPath = `${uploadYear}-${uploadMonth}-${uploadDate}/${deliveryId}`;
 
     const command = new PutObjectCommand({
